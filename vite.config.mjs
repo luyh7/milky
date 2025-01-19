465a7e257d377e9e1d7717d84267bf71fae82184
whisper <470103427@qq.com>
2025-01-19 19:40:46 +0800

    fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/milky',
+    base: './',
     optimizeDeps: {
         noDiscovery: true
     },
